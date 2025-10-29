03858f7cee6176b3f8af066abb73235b4acd7116
Dekker3D <dekker3d@gmail.com>
2025-10-29 14:05:31 +0100

    Fix camera pins for Freenove ESP32-S3 WROOM

--- src/sensors/camera.cpp
+++ src/sensors/camera.cpp
@@ -3,7 +3,7 @@
 #include "sensors/camera.h"
 #include "config.h"
 
-#define CAMERA_MODEL_AI_THINKER
+#define CAMERA_MODEL_ESP32S3_EYE
 #include "camera_pins.h" // load up ESP32-Cam pins, but allow switching to other models easily if needed.
 
 CameraClass::CameraClass() {
